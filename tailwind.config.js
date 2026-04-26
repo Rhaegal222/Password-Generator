619384d5d8637df7877e54634b6d3b8bf364cc21
Rhaegal222 <rhaegal222@gmail.com>
2026-04-26 15:52:23 +0200

    fix(build): make tailwind config standalone for docker production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import wrPreset from '../wyrmrest/ui-system/packages/ui-tailwind-preset/index.cjs';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    presets: [wrPreset],
     content: [
         './resources/**/*.blade.php',
         './resources/**/*.js',
